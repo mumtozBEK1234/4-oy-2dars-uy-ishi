56b76f308483337bb20a46211854597210bb552b
mumtozBEK1234 <email manzilingiz>
2024-05-20 15:12:07 +0300

    4 oy 2 dars uy ishi

---
+++ index.js
@@ -0,0 +1,266 @@
+// 1 - masala
+
+// let b = + prompt("son kiriting")
+
+// if (b > 0) {
+//     b++;
+
+//     console.log(b);
+// }
+
+
+// 2 - masala
+// let E = + prompt("son kiriting")
+// if (E > 0) {
+//     E++;
+// } else {
+//     E -= 2;
+// }
+// console.log(E);
+
+
+// 3 - masala
+
+// let B = + prompt("son kiriting")
+
+// if (B > 0) {
+//     B++;
+// } else if (B == 0) {
+//     B += 10;
+// } else {
+//     B -= 2;
+// }
+// console.log(B);
+
+
+// 4 - masala
+// let otvet = 0;
+// let v = + prompt("son kiriting");
+// if (v > 0) {
+//     javob++;
+// }
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     javob++;
+// }
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     javob++;
+// }
+// console.log(otvet);
+
+
+// 5-masala
+// let manfiy = 0;
+// let musbat = 0;
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     musbat++;
+// } else {
+//     manfiy++;
+// }
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     musbat++;
+// } else {
+//     manfiy++
+// }
+// let s = + prompt("son kiriting");
+// if (s > 0) {
+//     musbat++;
+// } else {
+//     manfiy++;
+// }
+
+// console.log("musbat");
+// console.log(musbat);
+// console.log("manfiy");
+// console.log(manfiy);
+
+
+// 6-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// if (a < b) {
+//     console.log(b);
+// } else {
+//     console.log(a);
+// }
+
+
+// 7-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// if (a > b) {
+//     console.log(b);
+// } else {
+//     console.log(a);
+// }
+
+
+// 8-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+// if (a < b) {
+// console.log(a);
+// }else{
+// console.log(b);
+// }
+
+
+// 9-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son kirit");
+
+// c = a - b;
+
+// d = b + a;
+
+// console.log("A son");
+// console.log(c);
+
+// console.log("B son");
+// console.log(d);
+
+
+// 10-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let c
+
+// if (a !== b){
+// c = a + b ;
+// console.log(c);
+// }else{
+// console.log(a);
+// console.log(b);
+// }
+
+
+// 11-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let c;
+
+// if (a == b){
+// console.log(a + b);
+// }
+
+// if(a > b){
+// console.log(a);
+// }
+
+// if (a < b){
+// console.log(b);
+// }
+
+
+// 12-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let c = + prompt("son");
+
+// if (a < b & a < c) {
+//     console.log(a);
+// }
+
+// if (b < a & b < c) {
+//     console.log(b);
+// }
+
+// if (c < a & c < b) {
+//     console.log(c);
+// }
+
+
+// 13-masala
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let c = + prompt("son");
+
+// if (a > b & a < c) {
+//     console.log(a);
+// }
+
+// if (b > a & b < c) {
+//     console.log(b);
+// }
+
+// if (c > a & c < b) {
+//     console.log(c);
+// }
+
+// if (c < a & c > b) {
+//     console.log(c);
+// }
+
+// if (b < a & b > c) {
+//     console.log(b);
+// }
+
+// if (a < b & a > c) {
+//     console.log(a);
+// }
+
+
+// 14-masala
+
+// let v = + prompt("son");
+
+// let a = + prompt("son");
+
+// let s = + prompt("son");
+
+// if (v < a & v < s) {
+//     console.log(a);
+// }
+
+// if (b < av& a < s) {
+//     console.log(b);
+// }
+
+// if (s < v & c < a) {
+//     console.log(c);
+// }
+
+// if (v > a & av> c) {
+
+//     console.log(a);
+// }
+
+// if (b > v & a > s) {
+//     console.log(b);
+// }
+
+// if (s > v & s > a) {
+//     console.log(c);
+// }
